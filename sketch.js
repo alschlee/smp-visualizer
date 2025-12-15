0ad9d8866a8ee07f89737babad7f77085e879289
alschlee <1daysixmeal@gmail.com>
2025-12-15 19:29:51 +0900

    [FEAT] UI 구현: 자동차

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+let roadY;
+let carX = 320;
+let carY;
+
 function setup() {
   createCanvas(800, 600);
+  roadY = height * 0.7;
+  carY = roadY - 40;
 }
 
 function draw() {
@@ -12,7 +18,6 @@ function draw() {
   }
 
   // 도로 그리기
-  let roadY = height * 0.7;
   noStroke();
   fill(40, 40, 50);
   rect(0, roadY, width, height - roadY);
@@ -24,4 +29,53 @@ function draw() {
     let x = (i * 60 - frameCount % 60) % width;
     line(x, roadY + 50, x + 30, roadY + 50);
   }
+
+  // 자동차 그리기
+  push();
+  translate(carX, carY);
+
+  noStroke();
+  fill(0, 0, 0, 100);
+  ellipse(0, 45, 80, 15);
+
+  fill(200, 10, 40);
+  rect(-40, 10, 80, 25, 5);
+
+  fill(220, 10, 35);
+  rect(-25, -15, 50, 25, 8, 8, 0, 0);
+
+  fill(100, 150, 200, 150);
+  rect(-20, -10, 15, 18, 3);
+  rect(5, -10, 15, 18, 3);
+
+  drawWheel(-20, 35, frameCount * 0.1);
+  drawWheel(20, 35, frameCount * 0.1);
+
+  pop();
+}
+
+function drawWheel(x, y, rotation) {
+  push();
+  translate(x, y);
+  rotate(rotation);
+
+  fill(30);
+  stroke(50);
+  strokeWeight(2);
+  ellipse(0, 0, 18, 18);
+
+  stroke(150);
+  strokeWeight(2);
+
+  for (let i = 0; i < 5; i++) {
+
+    let angle = (360 / 5) * i;
+    line(0, 0, cos(angle) * 6, sin(angle) * 6);
+  }
+
+  fill(100);
+  noStroke();
+  ellipse(0, 0, 6);
+
+  pop();
 }
